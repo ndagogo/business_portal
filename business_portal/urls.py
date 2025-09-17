bca3ebc56eb64536e3d2722ffdfd353f02d4b22e
ndagogo <ndagogo22@.com>
2025-09-17 04:07:24 +0100

    Initial commit - business_portal_project

---
+++ business_portal/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from portal import views  # Import your custom views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('portal.urls')),
+    # Authentication URLs
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
+    path('accounts/logout/', views.custom_logout, name='logout'),  # Use custom logout
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
